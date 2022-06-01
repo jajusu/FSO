8b3245ab1a81a6c4006a01bbbaec0e685af47834
J <j.suomaki@gmail.com>
2022-06-01 16:46:50 +0300

    1.7 valmis

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -2,34 +2,34 @@ import './App.css';
 
 import { useState } from 'react'
 
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const clickGood = () => {
-    setGood(good+ 1)
-  }
-
-  const clickNeutral = () => {
-    setNeutral(neutral+ 1)
-  }
-
-  const clickBad = () => {
-    setBad(bad+ 1)
-  }
+  let all=good+neutral+bad;
+  let average=(good*1+neutral*0+bad*-1)/all;
+  let positive=good/all;
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={clickGood}>good</button>
-      <button onClick={clickNeutral}>neutral</button>
-      <button onClick={clickBad}>bad</button>
+      <Button handleClick={() => setGood(good+1)} text="good"/>
+      <Button handleClick={() => setNeutral(neutral+1)} text="neutral"/>
+      <Button handleClick={() => setBad(bad+1)} text="bad"/>
       <h1>statistics</h1>
       good {good} <br></br>
       neutral {neutral} <br></br>
-      bad {bad}
+      bad {bad}<br></br>
+      all {all}<br></br>
+      average {average} <br></br>
+      positive {positive} %
     </div>
   )
 }
